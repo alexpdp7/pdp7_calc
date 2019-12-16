6cf92eace6e826daacd56295639da9ce186a580f
alex <alex@pdp7.net>
2019-12-16 14:28:34 +0100

    Make previous results clickable to insert in current expression

--- lib/main.dart
+++ lib/main.dart
@@ -21,27 +21,46 @@ class Calc extends StatefulWidget {
 
 class _CalcState extends State<Calc> {
   TextField textField;
+  TextEditingController textEditingController = new TextEditingController();
   List<String> values = [];
 
   _buildRow(int index) {
     if (index == values.length) {
-      return TextField(
+      textField = TextField(
         keyboardType: TextInputType.number,
         onSubmitted: _submitted,
         autofocus: true,
         textAlign: TextAlign.right,
         style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
+        controller: textEditingController,
       );
+      return textField;
     }
-    return Text(
-      values[index],
-      textAlign: TextAlign.right,
-      style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.75),
-    );
+    return FlatButton(
+        onPressed: () => _insert(values[index]),
+        child: Text(
+          values[index],
+          textAlign: TextAlign.right,
+          style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.75),
+        ));
+  }
+
+  _insert(String text) {
+    setState(() {
+      var newValue = textEditingController.text + text;
+      textEditingController.text += text;
+      textEditingController.value = TextEditingValue(
+        text: newValue,
+        selection: TextSelection.fromPosition(
+          TextPosition(offset: newValue.length),
+        ),
+      );
+    });
   }
 
   _submitted(String text) {
     setState(() {
+      textEditingController.text = "";
       try {
         var value =
             ExpressionEvaluator().eval(Expression.parse(text), {}).toString();
